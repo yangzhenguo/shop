6369365f35d27674a9033a7ef456e5d71914533f
Sam <sam@micous.com>
2019-03-12 11:29:15 +0800

    add gitignore

---
+++ util/src/test/java/com/hiyzg/shop/util/MD5UtilTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sam on 2019/3/12.
+ */
+public class MD5UtilTest {
+
+}
---
+++ web/src/main/java/com/hiyzg/shop/web/listeners/AppListener.java
@@ -0,0 +1,7 @@
+package com.hiyzg.shop.web.listeners;
+
+/**
+ * Created by Sam on 2019/3/12.
+ */
+public class AppListener {
+}
